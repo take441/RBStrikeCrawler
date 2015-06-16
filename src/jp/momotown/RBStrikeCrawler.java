ead4c99cf6f495692d6ccae320f7c46ffd92f60c
take441 <take441@gmail.com>
2015-06-16 15:36:40 +0900

    新規登録

---
+++ src/jp/momotown/RBStrikeCrawler.java
@@ -0,0 +1,14 @@
+package jp.momotown;
+
+public class RBStrikeCrawler {
+
+	public RBStrikeCrawler() {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
